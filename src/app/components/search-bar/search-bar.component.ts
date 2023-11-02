eb06204062a3f1e819ff186585c5f5f4ed264143
Lydie Chevalier <didof69@hotmail.fr>
2023-11-02 12:47:57 +0100

    3.8t - parametrage de la recherche codepostal / disparition du button au clic

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -9,28 +9,34 @@ import { LocationService } from 'src/app/services/location.service';
 })
 export class SearchBarComponent {
   @Output() location = new EventEmitter<Location>();
+  buttonClicked: boolean = false;
+
   locationsList!: Location[];
   filteredLocations: Location[] = [];
+  locationValue: string = '';
 
   constructor(private locationService: LocationService) {}
 
   ngOnInit() {
     this.locationService.getLocations().subscribe((locations) => {
       this.locationsList = [...locations];
-      console.log(this.locationsList);
+      // console.log(this.locationsList);
     });
   }
 
   //chercher par code postal commencant par la value
   onSearch(value: string) {
-    console.log(value);
+    // console.log(value);
+    this.buttonClicked = false;
     this.filteredLocations = this.locationsList.filter((e) =>
       e.zip_code.startsWith(value)
     );
   }
 
   getSearchedLocation(location: Location) {
-    // console.log('dans searchbar',location); 
+    // console.log('dans searchbar',location);
+    this.buttonClicked = true;
+    this.locationValue = `${location.zip_code} ${location.city_name}`;
     this.location.emit(location);
   }
 }
