807ab6daf0968fca028c780cddb48735699c5263
Lydie Chevalier <didof69@hotmail.fr>
2023-12-05 10:37:07 +0100

    8 - gestion erreur dans âge account

--- src/app/pages/page-account/page-account.component.ts
+++ src/app/pages/page-account/page-account.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MessageService } from 'primeng/api';
 import { User } from 'src/app/models/user';
 import { UserService } from 'src/app/services/user.service';
 
@@ -12,24 +13,29 @@ export class PageAccountComponent {
   isLog!: boolean;
   display: boolean = false;
 
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private messageService: MessageService
+  ) {}
 
   ngOnInit(): void {
     this.userService.isLog$.subscribe((resp) => {
       this.isLog = resp;
       if (this.isLog) {
-      this.userService.getUserByPseudo().subscribe({
-      next: (response) => {
-          this.user = response;
-          this.userService.isAdmin$.next(this.user.admin)         
-      },
-        error: (error) => {
-        //géréer l'erreur
-      },
+        this.userService.getUserByPseudo().subscribe({
+          next: (response) => {
+            this.user = response;
+            this.userService.isAdmin$.next(this.user.admin);
+          },
+          error: (error) => {
+            this.messageService.add({
+              severity: 'error',
+              summary: 'Chargement des données',
+              detail: 'Une erreur est survenue.',
+            });
+          },
+        });
+      }
     });
-    }
-    });
-
-    
   }
 }
